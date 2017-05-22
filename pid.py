097465f44cf3a34b56583f8c985f1d4acbd956a0
Denis Vitez <denis.vitez@gmail.com>
2017-05-22 21:43:21 +0200

    Changed PID parameters and slowed rotation speed.

--- pid.py
+++ pid.py
@@ -19,7 +19,7 @@ targetSpeed = 450
 #Init light sensor
 cl = ev3.ColorSensor()
 cl.mode='COL-REFLECT'
-blackValue = 10
+blackValue = 15
 #Init gyro
 gy = ev3.GyroSensor()
 #Gyro can be reset with changing between modes :) (WTF!)
@@ -28,12 +28,12 @@ gy.mode = 'GYRO-RATE'
 gy.mode='GYRO-ANG'
 #Init PID parameters --> tuned with Ziegler–Nichols method
 
-kP = 6
-kI = 30
-kD = 0.3
+kP = 3.6
+kI = 18
+kD = 0.18
 iterTime = 0.05
 bias = 0
-masterKoeficient = 1
+masterKoeficient = 0.99
 #masterKoeficient = 0.999
 #Some statistics
 sumError = 0
@@ -80,7 +80,6 @@ def goStraight(blackWait, skipLines):
             output = kP*error + kI*integral + kD*derivative + bias
             print("Output is: %d", (output))
             actualSpeed = targetSpeed + output
-            #client.publish("ev3/speed", actualSpeed);
             if(actualSpeed < 0):
                 actualSpeed = 0
             if(actualSpeed > maxR):
@@ -88,6 +87,7 @@ def goStraight(blackWait, skipLines):
             mR.run_forever(speed_sp=actualSpeed)
             mL.run_forever(speed_sp=targetSpeed)
             print("L/Rencoders: %d | %d --> difference in encoded steps is %d" % (mL.position, mR.position, error))
+            client.publish("ev3/speed", actualSpeed)
             sleep(iterTime)
     except KeyboardInterrupt:
         pass
@@ -99,11 +99,11 @@ def rotate(target, rotationSpeed):
             return
         diff = angle - target
         print("Angle diff is: "+str(diff))
-        targetSpeed = diff * 10
+        targetSpeed = diff * 5
         if diff > 0:
-            targetSpeed += 20
+            targetSpeed += 10
         if diff < 0:
-            targetSpeed -= 20
+            targetSpeed -= 10
         if(targetSpeed > 0 and targetSpeed > rotationSpeed):
             targetSpeed = rotationSpeed
         if(targetSpeed < 0 and targetSpeed < -rotationSpeed):
@@ -126,24 +126,24 @@ def readColor():
 if __name__ == "__main__":
     goStraight(10, 1)
     stop()
-    rotate(180, 200)
+    rotate(180, 100)
     stop()
     goStraight(10, 1)
-    rotate(0, 200)
+    rotate(0, 100)
     stop()
     goStraight(10, 2)
     stop()
-    rotate(180, 200)
+    rotate(180, 100)
     stop()
     goStraight(10, 2)
-    rotate(0, 200)
+    rotate(0, 100)
     stop()
     goStraight(10, 3)
     stop()
-    rotate(180, 200)
+    rotate(180, 100)
     stop()
     goStraight(10, 3)
-    rotate(0, 200)
+    rotate(0, 100)
     stop()
     #output statistics
     print("Finished running the programm.")
