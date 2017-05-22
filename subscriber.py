993fa19397e86e6b9ddf853d313fd0c5c6d9a4e8
1nfused <luka.golinar@gmail.com>
2017-05-22 16:15:33 +0200

    Added main check start.

--- subscriber.py
+++ subscriber.py
@@ -2,21 +2,24 @@ import paho.mqtt.client as mqtt
 
 
 def on_connect(client, userdata, rc):
-	client.subscribe("ev3/speed")
-	print("Subscribed to topic EV3")
+    client.subscribe("ev3/speed")
+    print("Subscribed to topic EV3")
+
 
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
     print("Topic: ", msg.topic+"\nMessage: "+str(msg.payload))
 
-client = mqtt.Client()
-client.on_connect = on_connect
-client.on_message = on_message
 
-client.connect("vitez.si", 1883, 60)
+if __name__ == "__main__":
+    client = mqtt.Client()
+    client.on_connect = on_connect
+    client.on_message = on_message
 
-# Blocking call that processes network traffic, dispatches callbacks and
-# handles reconnecting.
-# Other loop*() functions are available that give a threaded interface and a
-# manual interface.
-client.loop_forever()
+    client.connect("vitez.si", 1883, 60)
+
+    # Blocking call that processes network traffic, dispatches callbacks and
+    # handles reconnecting.
+    # Other loop*() functions are available that give a threaded interface and a
+    # manual interface.
+    client.loop_forever()
